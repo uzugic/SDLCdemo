75ef78cc88c7fd736d386fc25245fd99323be74f
Uros Zugic <uros.zugic@thomsonreuters.com>
2019-12-04 13:27:44 +0100

    added app

---
+++ src/main/java/com/sdlc/hello/HelloService.java
@@ -0,0 +1,16 @@
+package com.sdlc.hello;
+
+import java.sql.Timestamp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+	
+	@Bean
+	public void hello() {
+		System.out.println("Hello there friend! This is a DEMO application for the SDLC team.);
+	}
+
+}
---
+++ src/test/java/com/sdlc/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdlc.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
